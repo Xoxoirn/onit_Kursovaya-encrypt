cca933a4c3fe8454a2e63958dcbc8a4355115dcc
Xoxoirn <xoxoirnf@yandex.ru>
2022-03-31 03:46:00 +0300

    First

---
+++ Encryption.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kursovaya_ONIT_1
+{
+    public class Encryption
+    {
+        public string SourceFilePath { get; set; }
+        public string FormatFile { get; set; }
+        public string Key { set { changeKey(value); } }
+        /// <summary>
+        /// TRUE - DES
+        /// FALSE - AES
+        /// </summary>
+        public bool UseDES { get; set; }
+
+        public bool DeleteSource { get; set; }
+
+        private byte[] _keyAES;
+        private byte[] _keyDES;
+
+        public string GetNameFileWithoutFormat()
+        {
+            var tmp = SourceFilePath.Split('\\');
+            var name = tmp[tmp.Length - 1].Remove(tmp[tmp.Length - 1].LastIndexOf('.'));
+            return name;
+        }
+
+        public Encryption(string path, string key, bool DES = false)
+        {
+            SourceFilePath = path;
+            string[] tmp = path.Split('.');
+            FormatFile = tmp[tmp.Length - 1];
+            Key = key;
+            UseDES = DES;
+            DeleteSource = false;
+        }
+        public async Task<string> EncryptInFileAsync(string newFilePath)
+        {
+            return await Task.Run(() =>
+            {
+                byte[] encByte;
+                if (UseDES)
+                {
+                    encByte = encryptDES();
+                }
+                else
+                {
+                    encByte = encryptAES();
+                }
+                File.WriteAllBytes(newFilePath, encByte);
+                if(DeleteSource)
+                {
+                    File.Delete(SourceFilePath);
+                }
+                return newFilePath;
+            });
+        }
+        public async Task<string> DecryptInFileAsync(string newFilePath)
+        {
+            return await Task.Run(() =>
+            {
+                byte[] encByte;
+                if (UseDES)
+                {
+                    encByte = decryptDES();
+                }
+                else
+                {
+                    encByte = decryptAES();
+                }
+                File.WriteAllBytes(newFilePath, encByte);
+                File.Delete(SourceFilePath);
+                return newFilePath;
+            });
+        }
+
+        private void changeKey(string newKey)
+        {
+            SHA256 m = new SHA256CryptoServiceProvider();
+            _keyAES = m.ComputeHash(Encoding.ASCII.GetBytes(newKey));
+            generateKeyForDES();
+        }
+        private void generateKeyForDES()
+        {
+            _keyDES = new byte[8];
+            for(int i = 0; i < 8; i++)
+            {
+                _keyDES[i] = (byte)(_keyAES[i * 4] ^ _keyAES[i * 4 + 1] ^ _keyAES[i * 4 + 2] ^ _keyAES[i * 4 + 3]);
+            }
+        }
+
+        private byte[] encryptDES()
+        {
+            try
+            {
+                byte[] encrypted;
+                byte[] IV;
+
+                using (DES DESAlg = DES.Create())
+                {
+                    DESAlg.Mode = CipherMode.CFB;
+
+                    DESAlg.Key = _keyDES;
+
+                    DESAlg.GenerateIV();
+                    IV = DESAlg.IV;
+                    DESAlg.Padding = PaddingMode.PKCS7;
+
+                    var encryptor = DESAlg.CreateEncryptor(DESAlg.Key, DESAlg.IV);
+
+                    // Create the streams used for encryption. 
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            byte[] fileBytes = File.ReadAllBytes(SourceFilePath);
+                            csEncrypt.Write(fileBytes, 0, fileBytes.Length);
+                            csEncrypt.FlushFinalBlock();
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+
+                var combinedIvCt = new byte[IV.Length + encrypted.Length];
+                Array.Copy(IV, 0, combinedIvCt, 0, IV.Length);
+                Array.Copy(encrypted, 0, combinedIvCt, IV.Length, encrypted.Length);
+
+                return combinedIvCt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private byte[] decryptDES()
+        {
+            try
+            {
+                byte[] buff;
+
+                byte[] cipherTextCombined;
+                cipherTextCombined = File.ReadAllBytes(SourceFilePath);
+
+                using (DES DESAlg = DES.Create())
+                {
+                    DESAlg.Mode = CipherMode.CFB;
+                    DESAlg.Key = _keyDES;
+
+                    byte[] IV = new byte[DESAlg.BlockSize / 8];
+                    byte[] cipherText = new byte[cipherTextCombined.Length - IV.Length];
+
+                    Array.Copy(cipherTextCombined, IV, IV.Length);
+                    Array.Copy(cipherTextCombined, IV.Length, cipherText, 0, cipherText.Length);
+
+                    DESAlg.IV = IV;
+                    DESAlg.Padding = PaddingMode.PKCS7;
+
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform decryptor = DESAlg.CreateDecryptor(DESAlg.Key, DESAlg.IV);
+
+                    // Create the streams used for decryption. 
+                    using (var msDecrypt = new MemoryStream(cipherText))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            buff = new byte[cipherText.Length];
+                            csDecrypt.Read(buff, 0, buff.Length);
+                        }
+                    }
+                }
+                return buff;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private byte[] encryptAES()
+        {
+            try
+            {
+                byte[] encrypted;
+                byte[] IV;
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Mode = CipherMode.CFB;
+
+                    aesAlg.Key = _keyAES;
+
+                    aesAlg.GenerateIV();
+                    IV = aesAlg.IV;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+
+                    var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for encryption. 
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            byte[] fileBytes = File.ReadAllBytes(SourceFilePath);
+                            csEncrypt.Write(fileBytes, 0, fileBytes.Length);
+                            csEncrypt.FlushFinalBlock();
+                            encrypted = msEncrypt.ToArray();
+                        }
+                    }
+                }
+
+                var combinedIvCt = new byte[IV.Length + encrypted.Length];
+                Array.Copy(IV, 0, combinedIvCt, 0, IV.Length);
+                Array.Copy(encrypted, 0, combinedIvCt, IV.Length, encrypted.Length);
+
+                return combinedIvCt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private byte[] decryptAES()
+        {
+            try
+            {
+                byte[] buff;
+
+                byte[] cipherTextCombined;
+                cipherTextCombined = File.ReadAllBytes(SourceFilePath);
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Mode = CipherMode.CFB;
+
+                    aesAlg.Key = _keyAES;
+
+                    byte[] IV = new byte[aesAlg.BlockSize / 8];
+                    byte[] cipherText = new byte[cipherTextCombined.Length - IV.Length];
+
+                    Array.Copy(cipherTextCombined, IV, IV.Length);
+                    Array.Copy(cipherTextCombined, IV.Length, cipherText, 0, cipherText.Length);
+
+                    aesAlg.IV = IV;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for decryption. 
+                    using (var msDecrypt = new MemoryStream(cipherText))
+                    {
+                        using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                        {
+                            buff = new byte[cipherText.Length];
+                            csDecrypt.Read(buff, 0, buff.Length);
+                        }
+                    }
+                }
+                return buff;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,208 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Kursovaya_ONIT_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Encryption> EncryptionList = new List<Encryption>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            aesCheck.Checked += AesCheck_Checked; desCheck.Checked += DesCheck_Checked;
+        }
+
+        private void DesCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            aesCheck.IsChecked = false;
+        }
+
+        private void AesCheck_Checked(object sender, RoutedEventArgs e)
+        {
+            desCheck.IsChecked = false;
+        }
+
+        private void FileDrop(object sender, DragEventArgs e)
+        {
+            var paths = (string[])e.Data.GetData("FileDrop");
+            long sumByte = 0;
+            try
+            {
+                foreach (var item in paths)
+                {
+                    EncryptionList.Add(new Encryption(item, "0"));
+                    using (var stream = File.OpenRead(item))
+                    {
+                        sumByte += stream.Length;
+                        WriteLog("Файл " + item + $" загружен ({stream.Length} byte)");
+                    }
+                }
+                WriteLog("Всего файлов: " + paths.Length + $"({sumByte} byte)");
+                UpdQuery();
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message, Brushes.Red);
+            }
+        }
+
+        private void OpenClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileManager = new OpenFileDialog();
+            fileManager.ShowDialog();
+            var item = fileManager.FileName;
+            if (item != "")
+            {
+                EncryptionList.Add(new Encryption(item, "0"));
+                using (var stream = File.OpenRead(item))
+                {
+                    WriteLog("Файл " + item + $" загружен ({stream.Length} byte)");
+                }
+                UpdQuery();
+            }
+        }
+
+        private void WriteLog(string message, System.Windows.Media.SolidColorBrush color = null)
+        {
+            if (color == null)
+                color = System.Windows.Media.Brushes.Black;
+            var text = new TextBlock() { Text = message, Foreground = color };
+            Log.Items.Add(text);
+            Log.ScrollIntoView(text);
+            Log.SelectedItem = text;
+        }
+
+        private void UpdQuery()
+        {
+            int i = 0;
+            Query.Items.Clear();
+            foreach (var enc in EncryptionList)
+            {
+                TextBlock text;
+                if (i == 0)
+                {
+                    text = new TextBlock() { Text = enc.SourceFilePath, Foreground = System.Windows.Media.Brushes.DarkGreen };
+                }
+                else
+                {
+                    text = new TextBlock() { Text = enc.SourceFilePath, Foreground = System.Windows.Media.Brushes.Black };
+                }
+                Query.Items.Add(text);
+                i++;
+            }
+        }
+
+        private void EncryptClick(object sender, RoutedEventArgs e)
+        {
+            if (!CheckKeyRepeat())
+            {
+                MessageBox.Show("Пароли не совпадают", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var encFile = EncryptionList.FirstOrDefault();
+                SaveFileDialog fileManager = new SaveFileDialog();
+                fileManager.Filter = "Файлы enc|*.enc";
+                fileManager.FileName = encFile.GetNameFileWithoutFormat() + '.' + encFile.FormatFile + ".enc";
+                fileManager.FileOk += FileManager_EncryptGo;
+                fileManager.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message, Brushes.DarkRed);
+            }
+        }
+
+        private void DecryptClick(object sender, RoutedEventArgs e)
+        {
+            if (!CheckKeyRepeat())
+            {
+                MessageBox.Show("Пароли не совпадают", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            try
+            {
+                var encFile = EncryptionList.FirstOrDefault();
+                SaveFileDialog fileManager = new SaveFileDialog();
+                fileManager.FileName = encFile.GetNameFileWithoutFormat();
+                fileManager.FileOk += FileManager_DecryptGo;
+                fileManager.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message, Brushes.DarkRed);
+            }
+        }
+
+        private async void FileManager_DecryptGo(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+            var encFile = EncryptionList.FirstOrDefault();
+            var item = ((SaveFileDialog)sender).FileName;
+            try
+            {
+                if (item != "")
+                {
+                    encFile.Key = Key.Password;
+                    encFile.UseDES = desCheck.IsChecked.Value;
+
+                    await encFile.DecryptInFileAsync(item);
+                    EncryptionList.Remove(encFile);
+                }
+                WriteLog("Файл " + item + " успешно сохранен", Brushes.DarkBlue);
+                UpdQuery();
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message, Brushes.DarkRed);
+            }
+        }
+
+        private async void FileManager_EncryptGo(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var encFile = EncryptionList.FirstOrDefault();
+            var item = ((SaveFileDialog)sender).FileName;
+            try
+            {
+                if (item != "")
+                {
+                    encFile.Key = Key.Password;
+                    encFile.UseDES = desCheck.IsChecked.Value;
+
+                    await encFile.EncryptInFileAsync(item);
+                    EncryptionList.Remove(encFile);
+                }
+                WriteLog("Файл " + item + " успешно сохранен", Brushes.DarkBlue);
+                UpdQuery();
+            }
+            catch (Exception ex)
+            {
+                WriteLog(ex.Message, Brushes.DarkRed);
+            }
+        }
+
+        private bool CheckKeyRepeat()
+        {
+            if (Key.Password == RKey.Password)
+                return true;
+            return false;
+        }
+
+        private void ClearQuery(object sender, RoutedEventArgs e)
+        {
+            EncryptionList.Clear();
+            EncryptionList = new List<Encryption>();
+            UpdQuery();
+        }
+    }
+}
